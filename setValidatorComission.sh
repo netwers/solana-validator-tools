c35b23d336f6b0fcdebc5585fba864400bb3db36
netwers <netwers@freeton.one>
2024-02-16 17:32:08 +0300

    Update

---
+++ setValidatorComission.sh
@@ -0,0 +1,1 @@
+solana vote-update-commission vote-account-keypair.json 7 --keypair <keypair>
--- watchtower.sh
+++ watchtower.sh
@@ -2,27 +2,27 @@
 
 #
 # Solana Validator tool script by Netwers, 2022-2024.
-# 
+#
 # Do not forget to declare environment params for notifications first in $HOME/.profile:
 #  export TELEGRAM_BOT_TOKEN="0000000000:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
 #  export TELEGRAM_CHAT_ID="123456789"
 #  export TWILIO_CONFIG='ACCOUNT=<ACCOUNT_SID>,TOKEN=<ACCOUNT_KEY>,TO=<YOUR_PHONE_NUMBER>,FROM=<SENDER_PHONE_NUMBER>'
-# 
+#
 
 scriptPath=`cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P`
 source "${scriptPath}/env.sh"
 
 
         if [[ -z $1 ]]; then
-                echo "Validator identity unspecified. I gonna use $validatorVoteAccountPubKeyStaked from env.sh instead."
+                echo "Validator identity unspecified. I gonna use $validatorIdentityPubKeyStaked from env.sh instead."
                 echo "Usage: $0 <validator-identity-pubkey>"
-		validatorVoteAccountPubKey=$validatorVoteAccountPubKeyStaked
+                validatorIdentityPubKey=$validatorIdentityPubKeyStaked
         else
-                validatorVoteAccountPubKey=$1
+                validatorIdentityPubKey=$1
         fi
 
 echo ""
 date
 echo ""
 
-$execSolanaWatchtower --monitor-active-stake --validator-identity $validatorIdentityPubKey --minimum-validator-identity-balance 3 --interval 30
+$execSolanaWatchtower --monitor-active-stake --validator-identity $validatorIdentityPubKey --minimum-validator-identity-balance 1 --interval 30
