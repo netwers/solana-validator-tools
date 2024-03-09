5ac3a1bd3020d577dfc34e90befbe6b0e6522eda
netwers <netwers@freeton.one>
2024-03-10 01:10:05 +0300

    Some env.sh & dashboard fixes

--- getValidator.sh
+++ getValidator.sh
@@ -1,1 +1,13 @@
-solana validators --output=json | jq '.validators[] | select(.identityPubkey=="3kiyzZdvgkxhkef8v8cgbWe7JJ6a7NyNDpXMPnEUpb7x")'
+#!/usr/bin/env bash
+#
+# Solana Validator simple console "dashboard" script by Netwers, 2021-2024.
+#
+#
+
+scriptPath=`cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P`
+source "${scriptPath}/env.sh"
+echo
+date
+echo
+
+$execSolana validators --output=json | jq ".validators[] | select(.identityPubkey==\"$validatorIdentityPubKey\")"
