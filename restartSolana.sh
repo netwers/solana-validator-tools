523ebc4ad57d3cd915b94a613ebbf85162434f7c
netwers <netwers@freeton.one>
2024-03-13 21:25:31 +0300

    Validator restart script fix

--- restartSolana.sh
+++ restartSolana.sh
@@ -1,9 +1,16 @@
-#!/bin/bash
-echo ""
+#!/usr/bin/env bash
+#
+# Solana Validator simple console "dashboard" script by Netwers, 2021-2024.
+#
+#
+
+scriptPath=`cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P`
+source "${scriptPath}/env.sh"
+echo
 date
 echo "Please, do sudo =>"
 sudo echo "Thanks!"
 echo ""
 echo "Restarting solana..."
 echo ""
-solana-validator --ledger ~/snode/ledger/ wait-for-restart-window --max-delinquent-stake 5  --min-idle-time 70 && sudo systemctl stop solana.service && sudo systemctl start solana.service
+$execSolanaValidator --ledger $ledgerPath wait-for-restart-window --max-delinquent-stake 5  --min-idle-time 70 && sudo systemctl stop solana.service && sudo systemctl start solana.service
