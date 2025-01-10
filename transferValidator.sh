9c59ef275444b032eab1a69e7fb78f8fa4e27970
netwers <netwers@freeton.one>
2025-01-11 02:13:16 +0300

    Identity transition added

--- transferValidator.sh
+++ transferValidator.sh
@@ -7,11 +7,13 @@
 scriptPath=`cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P`
 source "${scriptPath}/env.sh"
 
+targetUsername="user"
 targetIPAddress="0.0.0.0"
 targetSSHport=22
+targetLedgerPath=$ledgerPath # Your remote machine's ledger path, e.g.: /home/user/solana/ledger/
 
 echo Trasnfering tower- file:
-/usr/bin/rsync -a -e "ssh -p $targetSSHport" $ledgerPath/tower-1_9-$validatorIdentityPubKeyStaked.bin $USER@$targetIPAddress:$ledgerPath/
+/usr/bin/rsync -a -e "ssh -p $targetSSHport" $ledgerPath/tower-1_9-$validatorIdentityPubKeyStaked.bin $targetUsername@$targetIPAddress:$targetLedgerPath/
 
 echo Setting new identity:
 ln -sf $keysPath/$validatorKeyFileUnstaked1 $keysPath/$validatorKeyFile
